f87b2333c9822909d7c1c84fe727758af7fba638
taisiinacio <taisi.inacio@gmail.com>
2021-03-21 21:17:03 -0300

    alteração2

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //alteração gitHub
+    //alteração2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
