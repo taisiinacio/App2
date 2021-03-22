f244178708deaaf467b04be0948a786a2e1b0202
taisiinacio <81122391+taisiinacio@users.noreply.github.com>
2021-03-21 21:13:16 -0300

    Update MainActivity.java

--- app/src/main/java/com/example/app2/MainActivity.java
+++ app/src/main/java/com/example/app2/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+//alteração gitHub
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
